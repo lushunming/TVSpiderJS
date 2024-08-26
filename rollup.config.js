7750c96700591b2f3ab04b1392fa2710f24c8287
lushunming <1357197829@qq.com>
2024-08-27 06:53:19 +0800

    插件

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@ import fs from 'node:fs'
 import path from 'node:path'
 import {fileURLToPath} from "node:url";
 import terser from '@rollup/plugin-terser';
+import {uglify} from "rollup-plugin-uglify";
+import del from 'rollup-plugin-delete'
 
 /*
 *
@@ -39,7 +41,7 @@ import terser from '@rollup/plugin-terser';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 let list = [];
-const jsFiles = ['jpyy.js','newvision.js', 'kankan70.js', 'tiantian.js', 'xb6v.js', 'mp4movie.js', 'jianpian.js', 'alipansou.js', 'huya.js', 'star.js', 'nivod.js', 'aiyingshi.js', 'sp360.js', 'mxanime.js', 'cntv.js', 'douban.js', 'pan_search.js', 'bilibili.js', 'changzhang.js', 'wogg.js', 'nangua.js', 'aliyunpanshare.js', 'dubo.js', 'haiwaikan.js', 'dygangs.js', 'cilixiong.js', 'lovemovie.js', 'gitcafe.js', 'kuaikan.js',];
+const jsFiles = ['jpyy.js', 'newvision.js', 'kankan70.js', 'tiantian.js', 'xb6v.js', 'mp4movie.js', 'jianpian.js', 'alipansou.js', 'huya.js', 'star.js', 'nivod.js', 'aiyingshi.js', 'sp360.js', 'mxanime.js', 'cntv.js', 'douban.js', 'pan_search.js', 'bilibili.js', 'changzhang.js', 'wogg.js', 'nangua.js', 'aliyunpanshare.js', 'dubo.js', 'haiwaikan.js', 'dygangs.js', 'cilixiong.js', 'lovemovie.js', 'gitcafe.js', 'kuaikan.js',];
 
 const sourceFolder = path.join(__dirname, '/js');
 const targetFolder_js = path.join(__dirname, '/dist');
@@ -52,10 +54,10 @@ function getConfig(sourceDir, targetDir) {
     files.forEach(file => {
         const sourcePath = path.join(sourceDir, file);
         const targetPath = path.join(targetDir, file);
-        if (jsFiles.includes(file) && !fs.existsSync(targetPath)) {
+        if (jsFiles.includes(file)) {   // && !fs.existsSync(targetPath)) {
             list.push({
                 input: sourcePath, output: {
-                    file: targetPath, format: 'es', plugins: [terser({toplevel:true})]
+                    file: targetPath, format: 'es', plugins: [terser(), del({targets: 'dist/' + file})]
                 },
 
             });
